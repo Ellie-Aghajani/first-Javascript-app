40c24a2d32a211519696f2ee37ec3805c458ff59
Zeynab Aghajani <zeynab.aghajani@hotmail.com>
2022-09-20 20:54:28 -0700

    I created pokemonlist variable, and assigned three pokemons to the list.

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+let pokemonList = [];
+pokemonList.push({name:"Squirtle", height:"0.5 m", type:"water"});
+pokemonList.push({name:"Butterfree", height:"1.1 m", type:"Bug"});
+pokemonList.push({name:"Pidgeot", height:"1.5 m", type:"Flying"});
