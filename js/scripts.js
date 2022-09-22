cb0e266ec040b40433893cced45d3ee548f04d66
Zeynab Aghajani <zeynab.aghajani@hotmail.com>
2022-09-21 17:40:25 -0700

    I used for to do part 2 of the task, and used if-else to do part 3 of the task.
    I used <br>  to make new lines.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,20 @@
 let pokemonList = [];
-pokemonList.push({name:"Squirtle", height:"0.5 m", type:"water"});
-pokemonList.push({name:"Butterfree", height:"1.1 m", type:"Bug"});
-pokemonList.push({name:"Pidgeot", height:"1.5 m", type:"Flying"});
+pokemonList.push({name:"Squirtle", height:0.5 , type:"water"});
+pokemonList.push({name:"Butterfree", height:1.1 , type:"Bug"});
+pokemonList.push({name:"Pidgeot", height:1.5 , type:"Flying"});
+console.log(pokemonList)
+//part 2
+let i = 0;
+for(let i=0; i< pokemonList.length; i++){
+    document.write(pokemonList[i].name+" (Height: "+pokemonList[i].height+")"+"<br>");
+}
+document.write("<br><br>part 3<br><br>")
+//part 3
+for(let i=0; i< pokemonList.length; i++){
+    if(i > 1.4){
+        document.write(pokemonList[i].name+"  (Height: "+pokemonList[i].height+")- Wow, that's big!"+"<br>");
+    }else{
+        document.write(pokemonList[i].name+"  (Height: "+pokemonList[i].height+")"+"<br>");
+    }
+    
+}
