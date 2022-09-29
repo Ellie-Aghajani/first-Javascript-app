e24a3ff4b95fe7a48ce5c17c4e5e63b28da5e2ee
Zeynab Aghajani <zeynab.aghajani@hotmail.com>
2022-09-28 18:12:19 -0700

    Update scripts.js
    I used IIFE, and rewrote the code.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,39 @@
-let pokemonList = [];
-pokemonList.push({name:"Squirtle", height:0.5 , type:"water"});
-pokemonList.push({name:"Butterfree", height:1.1 , type:"Bug"});
-pokemonList.push({name:"Pidgeot", height:1.5 , type:"Flying"});
-console.log(pokemonList)
-//part 2
-let i = 0;
-for(let i=0; i< pokemonList.length; i++){
-    document.write(pokemonList[i].name+" (Height: "+pokemonList[i].height+")"+"<br>");
-}
-document.write("<br><br>part 3<br><br>")
-//part 3
-for(let i=0; i< pokemonList.length; i++){
-    if(i > 1.4){
-        document.write(pokemonList[i].name+"  (Height: "+pokemonList[i].height+")- Wow, that's big!"+"<br>");
-    }else{
-        document.write(pokemonList[i].name+"  (Height: "+pokemonList[i].height+")"+"<br>");
+let pokemonRepository =(function(){
+    let pokemonList = [
+        {name:"Squirtle", height:0.5 , type:"water"},
+        {name:"Butterfree", height:1.1 , type:["Bug","flying"]},
+        {name:"Pidgeot", height:1.5 , type:["Flying","normal"]}
+    ]
+
+    function getAll() {
+        return pokemonList;
+      }
+
+      function add(pokemon) { 
+        if (typeof pokemon === 'object' && 'name' in pokemon){
+        pokemonList.push(pokemon);
+      }
     }
-    
-}
+
+      return {
+        add: add,
+        getAll: getAll
+      }
+
+})()
+
+pokemonRepository.add({name:'Furret', height: 1.8, type:'normal'});
+
+
+pokemonRepository.getAll().forEach(function(pokemon){
+
+    if(pokemon.height >= 1.6) {
+        document.write(pokemon.name + " (height: " + pokemon.height + " m) - Wow, that is big!" + "<br>")
+    }
+    else if (pokemon.height >= 1.5 && pokemon.height < 1.6){
+        document.write(pokemon.name + " (height: " + pokemon.height + " m) - That is a medium pokemon!" + "<br>")
+    }
+    else {
+        document.write(pokemon.name + " (height: " + pokemon.height + " m)- That is a small pokemon!" + "<br>")
+    }  
+})
